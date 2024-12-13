ebfcad8b3a6228d22825db9a81974a81ed81695a
dustinmclean <148822306+dustinmclean@users.noreply.github.com>
2024-12-13 13:35:30 -0800

    create accordion for Process using CSS and Javascript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("fuck you");
+// Ensuring only 1 Process accordion tab can be opened at a time:
+
+document.querySelectorAll(".tab-checkbox").forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      document.querySelectorAll(".tab-checkbox").forEach((otherCheckbox) => {
+        if (otherCheckbox !== this) {
+          otherCheckbox.checked = false;
+        }
+      });
+    }
+  });
+});
