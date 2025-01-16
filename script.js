65d884355e12eb12d85b85f03a97279d026f5a4f
dustinmclean <148822306+dustinmclean@users.noreply.github.com>
2025-01-15 17:42:30 -0800

    create a whole bunch of stuff, create the contact form using emailjs

--- script.js
+++ script.js
@@ -27,3 +27,97 @@ document.querySelectorAll(".pricing-checkbox").forEach((checkbox) => {
 });
 
 console.log("crap your drawers");
+
+// -----------------------------   Contact Form   --------------------------- //
+
+// Prevent Default and Call validateForm //
+
+document.getElementById("myForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateForm();
+});
+
+// Query Selectors On Contact Form //
+
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+const nameContainer = document.querySelector(".name-container");
+const emailContainer = document.querySelector(".email-container");
+const messageSection = document.querySelector(".message-section");
+
+// Form Validation Logic //
+
+function validateForm() {
+  let errorFlag = false;
+
+  if (nameInput.value.length < 1) {
+    errorNodes[0].innerText = "Name must not blank";
+    nameContainer.classList.add("name-container-error");
+    errorFlag = true;
+  }
+
+  if (!emailIsValid(email.value)) {
+    errorNodes[1].innerText = "Invalid email address";
+    emailContainer.classList.add("name-container-error");
+    errorFlag = true;
+  }
+
+  if (message.value.length < 1) {
+    errorNodes[2].innerText = "Message must not be blank";
+    messageSection.classList.add("message-error-border");
+    errorFlag = true;
+  }
+
+  if (!errorFlag) {
+    success.innerText = "Your message was successfully submitted!";
+    nameInput.innerText = "";
+
+    emailjs
+      .send("service_8rfhzvw", "template_l02d47g", {
+        name: nameInput.value,
+        email: emailInput.value,
+        message: messageInput.value,
+      })
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          success.innerText = "Your message has been sent!";
+        },
+        (error) => {
+          console.error("FAILED...", error);
+          success.innerText = "Failed to send your message. Please try again.";
+        }
+      );
+  }
+}
+
+// Clear Error Message in NAME CONTAINER //
+
+nameContainer.addEventListener("click", () => {
+  errorNodes[0].innerText = "";
+  nameContainer.classList.remove("name-container-error");
+});
+
+// Clear Error Messages in EMAIL CONTAINER //
+
+emailContainer.addEventListener("click", () => {
+  errorNodes[1].innerText = "";
+  emailContainer.classList.remove("name-container-error");
+});
+
+// Clear Error Messages in MESSAGE CONTAINER //
+
+messageSection.addEventListener("click", () => {
+  errorNodes[2].innerText = "";
+  messageSection.classList.remove("message-error-border");
+});
+
+// Check Using A Regular Expression Pattern For Email Validation //
+
+function emailIsValid(email) {
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
+}
